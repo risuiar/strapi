3c9e5456049323d8ac29a2147605671d5c3b1306
Ricardo Vögeli <rvoegeli@gmail.com>
2025-11-28 14:26:59 +0100

    fixes

--- config/server.js
+++ config/server.js
@@ -1,12 +1,19 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-  url: "https://" + env("APP_URL", "localhost"),
-  allowedHosts: [env("APP_URL", "http://localhost")],
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-  },
-});
+module.exports = ({ env }) => {
+  const url = env("URL", "http://localhost:1337");
+  // Extract host from URL (remove protocol and port if present)
+  const host = url.replace(/^https?:\/\//, "").replace(/:\d+$/, "");
+
+  return {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+      keys: env.array("APP_KEYS"),
+    },
+    url: url,
+    proxy: env.bool("PROXY", true),
+    allowedHosts: [host, "localhost"],
+    webhooks: {
+      populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    },
+  };
+};
