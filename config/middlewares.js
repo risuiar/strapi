444257b9eb7e514e39de3b2a045e068b9d4fec56
Ricardo Vögeli <rvoegeli@gmail.com>
2025-11-30 19:53:46 +0100

    s3 configured

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,32 @@ module.exports = [
     name: 'global::root-redirect',
     config: {},
   },
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            's3.eu-north-1.amazonaws.com',
+            '*.s3.eu-north-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            's3.eu-north-1.amazonaws.com',
+            '*.s3.eu-north-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.js
+++ config/plugins.js
@@ -17,4 +17,22 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+        region: env("AWS_REGION", "eu-north-1"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
